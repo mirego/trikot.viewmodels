dac724896c0a503fd64b44e17d1eb16cd2016962
Charles MacDuff <cmacduff@mirego.com>
2019-10-16 16:43:54 -0400

    Verify if selector has any value before applying background

--- android-ktx/src/main/java/com/mirego/trikot/metaviews/MetaButtonBinder.kt
+++ android-ktx/src/main/java/com/mirego/trikot/metaviews/MetaButtonBinder.kt
@@ -151,24 +151,28 @@ object MetaButtonBinder {
 
             it.backgroundColor.asLiveData()
                 .observe(lifecycleOwnerWrapper.lifecycleOwner) { selector ->
-                    val defaultColor =
-                        Color.parseColor(selector.default?.hexARGB("#") ?: "#00000000")
-                    val hoveredColor =
-                        selector.highlighted?.let { Color.parseColor(it.hexARGB("#")) }
-                            ?: defaultColor
-                    val selectedColor =
-                        selector.selected?.let { Color.parseColor(it.hexARGB("#")) } ?: defaultColor
-                    val disabledColor =
-                        selector.disabled?.let { Color.parseColor(it.hexARGB("#")) } ?: defaultColor
-                    button.backgroundTintList = ColorStateList(
-                        arrayOf(
-                            intArrayOf(R.attr.state_enabled),
-                            intArrayOf(R.attr.state_hovered),
-                            intArrayOf(R.attr.state_selected),
-                            intArrayOf(-R.attr.state_enabled)
-                        ),
-                        intArrayOf(defaultColor, hoveredColor, selectedColor, disabledColor)
-                    )
+                    if (selector.hasAnyValue) {
+                        val defaultColor =
+                            Color.parseColor(selector.default?.hexARGB("#") ?: "#00000000")
+                        val hoveredColor =
+                            selector.highlighted?.let { Color.parseColor(it.hexARGB("#")) }
+                                ?: defaultColor
+                        val selectedColor =
+                            selector.selected?.let { Color.parseColor(it.hexARGB("#")) }
+                                ?: defaultColor
+                        val disabledColor =
+                            selector.disabled?.let { Color.parseColor(it.hexARGB("#")) }
+                                ?: defaultColor
+                        button.backgroundTintList = ColorStateList(
+                            arrayOf(
+                                intArrayOf(R.attr.state_enabled),
+                                intArrayOf(R.attr.state_hovered),
+                                intArrayOf(R.attr.state_selected),
+                                intArrayOf(-R.attr.state_enabled)
+                            ),
+                            intArrayOf(defaultColor, hoveredColor, selectedColor, disabledColor)
+                        )
+                    }
                 }
 
             it.textColor.asLiveData()
