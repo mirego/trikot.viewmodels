41d37b5f703c2b0320eb00dd523f5e4b6a6128f8
Marc-Antoine <magiguere@mirego.com>
2020-05-14 10:18:41 -0400

    Add doc

--- viewModels/src/commonMain/kotlin/com/mirego/trikot/viewModels/SliderViewModel.kt
+++ viewModels/src/commonMain/kotlin/com/mirego/trikot/viewModels/SliderViewModel.kt
@@ -3,10 +3,25 @@ package com.mirego.trikot.viewmodels
 import org.reactivestreams.Publisher
 
 interface SliderViewModel: ViewModel {
+    /**
+     * Minimum value of the slider
+     */
     val minValue: Int
+    /**
+     * Maximum value of the slider
+     */
     val maxValue: Int
+    /**
+     * Amount to increase every step
+     */
     val stepValue: Int
+    /**
+     * Currently selected value on the slider
+     */
     val selectedValue: Publisher<Int>
 
+    /**
+     * Set the value selected by the platform slider
+     */
     fun setSelectedValue(value: Int)
 }
