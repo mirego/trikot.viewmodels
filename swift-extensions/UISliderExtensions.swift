d57832c093fb6e926cc19d7dbf0e128b0bcd6a54
Marc-Antoine <magiguere@mirego.com>
2020-05-14 09:54:40 -0400

    Fix swift extension

--- swift-extensions/UISliderExtensions.swift
+++ swift-extensions/UISliderExtensions.swift
@@ -1,19 +1,10 @@
-
 import UIKit
 import TRIKOT_FRAMEWORK_NAME
 
-class Slider : UISlider {
+public class Slider : UISlider {
     private var oldValue: Int32 = 0
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    var sliderViewModel: SliderViewModel? {
+    
+    public var sliderViewModel: SliderViewModel? {
         get { return trikotViewModel() }
         set(value) {
             viewModel = value
@@ -21,9 +12,9 @@ class Slider : UISlider {
                 self.minimumValue = Float(sliderViewModel.minValue)
                 self.maximumValue = Float(sliderViewModel.maxValue)
 
-                self.addTarget(self, action: #selector(UISlider.onValueChanged), for: .valueChanged)
+                self.addTarget(self, action: #selector(Slider.onValueChanged), for: .valueChanged)
                 observe(sliderViewModel.selectedValue) { [weak self] (value: Int32) in
-                    self?.slider.value = Float(value)
+                    self?.value = Float(value)
                 }
             }
         }
@@ -34,10 +25,11 @@ class Slider : UISlider {
         guard let integerValue = Int32(String(format: "%.0f", self.value)) else { return }
 
         if integerValue != oldValue {
-            UIImpactFeedbackGenerator(style: .light).impactOccurred()
+            if #available(iOS 10.0, *) {
+                UIImpactFeedbackGenerator(style: .light).impactOccurred()
+            }
             oldValue = integerValue
             sliderViewModel?.setSelectedValue(value: integerValue)
         }
     }
 }
-
