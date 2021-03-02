8fbf10a3b0c787ca9f50db3776b165f5d21d1dd7
Marc Lefrancois <mlefrancois@mirego.com>
2021-03-02 09:22:39 -0500

    Add a jenkins job so we can build from the legacy 1.0.x branch

--- jenkins-jobs/plugin_release.groovy
+++ jenkins-jobs/plugin_release.groovy
@@ -44,3 +44,81 @@ job(context.jobFullName) {
         }
     }
 }
+
+job("$context.jobFullName-legacy-1_0_*") {
+    description("Make a release of trikot.viewmodels")
+    logRotator(5)
+    scm {
+        git {
+            branch("release/1.0.x")
+            remote {
+                name('origin')
+                url("${GIT_URL}")
+                credentials('github')
+            }
+        }
+    }
+    wrappers {
+        credentialsBinding {
+            amazonWebServicesCredentialsBinding {
+                accessKeyVariable('MAVEN_AWS_KEY')
+                secretKeyVariable('MAVEN_AWS_SECRET')
+                credentialsId('mirego-maven-aws')
+            }
+        }
+    }
+    steps {
+        gradle {
+            useWrapper()
+            makeExecutable()
+            tasks(':viewmodels:release')
+            switches('-i -s')
+        }
+    }
+    publishers {
+        slackNotifier {
+            notifyFailure(true)
+            notifyBackToNormal(true)
+            room(context.slackChannel)
+        }
+    }
+}
+
+job("$context.jobFullName-android-ktx-legacy-1_0_*") {
+    description("Make a release of trikot.viewmodels:android-ktx")
+    logRotator(5)
+    scm {
+        git {
+            branch("release/1.0.x")
+            remote {
+                name('origin')
+                url("${GIT_URL}")
+                credentials('github')
+            }
+        }
+    }
+    wrappers {
+        credentialsBinding {
+            amazonWebServicesCredentialsBinding {
+                accessKeyVariable('MAVEN_AWS_KEY')
+                secretKeyVariable('MAVEN_AWS_SECRET')
+                credentialsId('mirego-maven-aws')
+            }
+        }
+    }
+    steps {
+        gradle {
+            useWrapper()
+            makeExecutable()
+            tasks(':android-ktx:release')
+            switches('-i -s')
+        }
+    }
+    publishers {
+        slackNotifier {
+            notifyFailure(true)
+            notifyBackToNormal(true)
+            room(context.slackChannel)
+        }
+    }
+}
