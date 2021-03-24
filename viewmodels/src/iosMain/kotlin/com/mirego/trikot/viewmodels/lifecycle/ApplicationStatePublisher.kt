d494a46cd33e02c92a6ad56a5258e794d8517afb
Antoine Lamy <antoinelamy@gmail.com>
2021-03-24 10:29:41 -0400

    Revert "Add observers on init"
    This reverts commit 3ba0f284df10c7779cfd028f00bfe6b6a80e347a.

--- viewmodels/src/iosMain/kotlin/com/mirego/trikot/viewmodels/lifecycle/ApplicationStatePublisher.kt
+++ viewmodels/src/iosMain/kotlin/com/mirego/trikot/viewmodels/lifecycle/ApplicationStatePublisher.kt
@@ -58,7 +58,8 @@ actual class ApplicationStatePublisher :
     private class ApplicationStateObserver : NSObject() {
         private var callback: ((ApplicationState) -> Unit)? by atomic(null)
 
-        init {
+        fun start(closure: (ApplicationState) -> Unit) {
+            callback = closure.freeze()
             NSNotificationCenter.defaultCenter.addObserver(
                 this,
                 sel_registerName("willEnterForeground"),
@@ -73,12 +74,9 @@ actual class ApplicationStatePublisher :
             )
         }
 
-        fun start(closure: (ApplicationState) -> Unit) {
-            callback = closure.freeze()
-        }
-
         fun stop() {
             callback = null
+            NSNotificationCenter.defaultCenter.removeObserver(this)
         }
 
         @ObjCAction
