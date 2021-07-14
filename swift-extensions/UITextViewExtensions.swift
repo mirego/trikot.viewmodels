bff360bdb92e13dc52acee91e9eae6a3935e9e26
steven <steven.detilly@gmail.com>
2021-07-14 13:51:35 -0400

    PR comment: add support for RichText for UITextView

--- swift-extensions/UITextViewExtensions.swift
+++ swift-extensions/UITextViewExtensions.swift
@@ -7,7 +7,14 @@ extension UITextView {
         set(value) {
             viewModel = value
             if let labelViewModel = value {
-                bind(labelViewModel.text, \UITextView.text)
+                if let richText = labelViewModel.richText, let font = self.font {
+                    observe(richText) {[weak self] (richText: RichText) in
+                        guard let self = self else { return }
+                        self.attributedText = self.richTextToAttributedString(richText, referenceFont: font)
+                    }
+                } else {
+                    bind(labelViewModel.text, \UITextView.text)
+                }
                 bindColorSelectorDefaultValue(labelViewModel.textColor, \UITextView.textColor)
             }
         }
